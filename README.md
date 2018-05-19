c9367175c7c417dcf76ef5a47684c7526cf7a066
dorinmoldovan <dorin.moldovan@yahoo.com>
2018-05-20 02:30:45 +0300

    final version

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # NewsApp
 A News App that gives regularly-updated news about Cluj-Napoca, Romania. The project is part of the "Android Basics Nanodegree by Google - Udacity". (#madewithudacity #googleudacityscholars #fendproject)
+
+<p align="center">
+  <img src="device-2018-05-20.png" />
+</p>
--- app/src/main/java/com/androidbasicsnd/android/newsapp/MainActivity.java
+++ app/src/main/java/com/androidbasicsnd/android/newsapp/MainActivity.java
@@ -1,13 +1,99 @@
 package com.androidbasicsnd.android.newsapp;
 
+import android.app.LoaderManager;
+import android.content.Context;
+import android.content.Intent;
+import android.content.Loader;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<List<News>> {
+
+    private static final int NEWS_LOADER_ID = 1;
+
+    public static final String LOG_TAG = MainActivity.class.getName();
+
+    private NewsAdapter newsAdapter;
+
+    private TextView emptyStateTextView;
+
+    private static final String GUARDIAN_API_URL = "https://content.guardianapis.com/search?q=cluj-napoca&api-key=test";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView newsListView = (ListView) findViewById(R.id.list);
+
+        emptyStateTextView = (TextView) findViewById(R.id.empty_view);
+        newsListView.setEmptyView(emptyStateTextView);
+        newsAdapter = new NewsAdapter(this, new ArrayList<News>());
+
+        newsListView.setAdapter(newsAdapter);
+
+        newsListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+
+                News currentNews = newsAdapter.getItem(position);
+
+                Uri newsUri = Uri.parse(currentNews.getUrl());
+
+                Intent websiteIntent = new Intent(Intent.ACTION_VIEW, newsUri);
+
+                startActivity(websiteIntent);
+            }
+        });
+
+        ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+
+        if (networkInfo != null && networkInfo.isConnected()) {
+            LoaderManager loaderManager = getLoaderManager();
+            loaderManager.initLoader(NEWS_LOADER_ID, null, this);
+        } else {
+            View loadingIndicator = findViewById(R.id.loading_indicator);
+            loadingIndicator.setVisibility(View.GONE);
+            emptyStateTextView.setText(R.string.no_internet_connection);
+        }
     }
+
+
+    @Override
+    public Loader<List<News>> onCreateLoader(int i, Bundle bundle) {
+        return new NewsLoader(this, GUARDIAN_API_URL);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<List<News>> loader, List<News> newsList) {
+
+        View loadingIndicator = findViewById(R.id.loading_indicator);
+        loadingIndicator.setVisibility(View.GONE);
+
+        emptyStateTextView.setText(R.string.no_news);
+
+        newsAdapter.clear();
+
+        if (newsList != null && !newsList.isEmpty()) {
+            newsAdapter.addAll(newsList);
+        }
+    }
+
+    @Override
+    public void onLoaderReset(Loader<List<News>> loader) {
+        newsAdapter.clear();
+    }
+
 }
